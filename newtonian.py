ac8435476e88f01ea1d6899c5596eab277463b7a
Nick Austin <295184097+nicklovesphysics@users.noreply.github.com>
2026-07-18 11:03:34 -0400

    added vectors for simulation, fixed typos

--- newtonian.py
+++ newtonian.py
@@ -8,29 +8,30 @@ import matplotlib.pyplot as plt
 class MassiveBody:
 
     def __init__(self, mass, radius):
-        self.shape = mass
+        self.mass = mass
         self.radius = radius
     
    
     
 class Photon(MassiveBody):
 
-    def __init__(self, mass, radius, velocity, direction):
+    def __init__(self, mass, radius, velocity=list):
         super().__init__(mass, radius)          #assigning tiny masses and radii to a photon to mimic massless tiny particle.
         self.velocity = velocity
-        self.direction = direction
+
 
 class CentralMass(MassiveBody):
     def __init__(self, mass, radius):
         super().__init__(mass,radius)
         self.position = np.array(0,0)
+        self.velocity = 0
 
 
 #====================================================
-#Letting massive bodies interact
+#Letting massive bodies interact ---   COMPONENT-WISE COMPUTATION FOR A VECTOR OUTPUT
 #====================================================
 
-def Radius(obj1_pos, obj2_pos):
+def Distance(obj1_pos, obj2_pos):
     xrad = obj1_pos[0]-obj2_pos[0]
     yrad = obj1_pos[1]-obj2_pos[1]
     dist_norm = np.sqrt((xrad**2) + (yrad**2))
@@ -38,18 +39,19 @@ def Radius(obj1_pos, obj2_pos):
 
 class Gravity:
 
-    g = (6.67*(10**(-11)))
+    g_const = (6.67*(10**(-11)))
 
-    def direction(obj1_pos, obj2_pos):
+    def Direction(obj1_pos, obj2_pos):
         xrad = obj1_pos[0]-obj2_pos[0]
         yrad = obj1_pos[1]-obj2_pos[1]      #find positional vectors
-
-        #normalize here
+        radius = Distance(obj1_pos,obj2_pos)
+        rad_x = radius[0]
+        rad_y = radius[1]
 
         return np.array(xrad, yrad)
     
     
-    def force(obj1, obj2):
+    def Force(obj1, obj2, g=g_const):
         rad = Radius(obj1.position(), obj2.position())
         F = (g*obj1.mass()*obj2.mass())/(rad)
         return F
